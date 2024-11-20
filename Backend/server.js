e5e0260f47d6095626ff0d2645ab5789c6fee6d7
Victor Nogueira <vgdsno@gmail.com>
2024-11-20 01:54:41 -0300

    teste

--- Backend/server.js
+++ Backend/server.js
@@ -7,18 +7,7 @@ require('dotenv').config();
 
 const app = express();
 
-// Definir as origens permitidas para CORS
-const corsOptions = {
-  origin: ['http://localhost:3003', 'https://prodify-ruddy.vercel.app'], // Permitir apenas origens específicas
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type'],
-  credentials: true, // Permitir envio de cookies, se necessário
-  preflightContinue: false, // Não permitir que o CORS faça redirecionamento
-  optionsSuccessStatus: 204, // Definir código de sucesso para as requisições OPTIONS
-};
-
-// Aplicando o middleware de CORS
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Aumentar o limite do body-parser
 app.use(bodyParser.json({ limit: '50mb' }));
